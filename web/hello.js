3c88760e8bcfd4fbe81d0a00820c136e0e0d3eae
yuvallinux <yuvddd05@gmail.com>
2024-06-11 23:15:53 +0300

    11.6.2024 23:00

--- web/hello.js
+++ web/hello.js
@@ -1,3 +1,13 @@
+function formatString(template, ...args){
+    return template.replace(/{([0-9]+)}/g, function (match, index) {
+      return typeof args[index] === 'undefined' ? match : args[index];
+    });
+}
+
 for (let i = 0; i < 10; ++i){
+    if ( i % 2 == 0)
+        console.log(formatString('Hello World, i is {0}', i));
+    else
     console.log('Hello World');
+
 }
