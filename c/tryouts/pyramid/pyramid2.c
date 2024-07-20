8f7951a496a9dd40f8394c1cb214673822f0859f
yuvallinux <yuvddd05@gmail.com>
2024-07-20 23:02:06 +0300

    20.7.2024 23:00

--- c/tryouts/pyramid/pyramid2.c
+++ c/tryouts/pyramid/pyramid2.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     for (i=1;i<=n;i++)
     {
         print_pyramid(i);
-        //printf("*");
+        printf("*");
 
         //if (i != n)
         //{
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
     for (i=n-1;i>=1;i--)
     {
         print_pyramid(i);
-        //printf("*");
+        printf("*");
     }
 
     printf("\n");
@@ -43,11 +43,11 @@ void print_pyramid(int num)
 
     for (i=1;i<num;i++)
     {
-        //printf("%d", i);
+        printf("%d", i);
     }
 
     for (i=num;i>=1;i--)
     {
-        //printf("%d", i);
+        printf("%d", i);
     }
 }
--- c/tryouts/pyramid/pyramid3.c
+++ c/tryouts/pyramid/pyramid3.c
@@ -10,7 +10,7 @@ typedef unsigned int uint;
 
 int main(int argc, char *argv[])
 {
-    uint n = atoi(argv[argc -1 ]);
+    int n = atoi(argv[argc -1 ]);
     uint len = len_till_number(n - 1);
     
     char *forward = malloc((len + 1) * sizeof(char));
@@ -22,12 +22,14 @@ int main(int argc, char *argv[])
 
     for (int i=n-1;i>=1;i--)
     {
-        char curr_number[32];
+        char curr_number[16];
 
         sprintf(curr_number, "%d", i);
         strcat(end_reverse, curr_number);
-        end_reverse = end_reverse + strlen(curr_number);    
+        end_reverse += strlen(curr_number);    
     }
+
+    *end_reverse = '\0';
 
     printf("*");
 
@@ -35,14 +37,16 @@ int main(int argc, char *argv[])
     {
         printf("%s%d%s*", forward, i, end_reverse);
 
-        char curr_number[32];
+        char curr_number[16];
         sprintf(curr_number, "%d", i);
-        end_reverse = end_reverse - strlen(curr_number);
+
+        uint curr_len = strlen(curr_number);
+        end_reverse -= curr_len;
 
         if (i != n)
         {
             strcat(end_forward, curr_number);
-            end_forward = end_forward + strlen(curr_number);    
+            end_forward += curr_len;    
         }
     }
 
@@ -50,16 +54,15 @@ int main(int argc, char *argv[])
 
     for (uint i=n-1;i>=1;i--)
     {
-        char curr_number[32];
+        char curr_number[16];
         sprintf(curr_number, "%d", i);
+        uint curr_len = strlen(curr_number);
 
-        end_forward = end_forward - strlen(curr_number);
-
+        end_forward = end_forward - curr_len;
         *end_forward = '\0';  
 
         printf("%s%s*", forward, end_reverse);
-        end_reverse = end_reverse + strlen(curr_number);
- 
+        end_reverse = end_reverse + curr_len;
     }
 
     free(reverse);
