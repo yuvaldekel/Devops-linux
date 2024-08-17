b4abf3cf7d43bfdd291fcae20f15b9101e2a00dd
yuvallinux <yuvddd05@gmail.com>
2024-08-17 18:15:08 +0300

    delete Terraform

--- Azure/Terraform/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_resource_group" "rg" {
-  location = "east us"
-  name     = "rg1"
-}
-
-resource "azurerm_resource_group" "second" {
-  location = "east us"
-  name     = "rg2"
-}
