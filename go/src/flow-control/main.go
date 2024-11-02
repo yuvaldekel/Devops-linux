b0da0d21043925cd6cd087afac7255621d6bf5b9
yuvallinux <yuvddd05@gmail.com>
2024-11-02 18:50:29 +0200

    19:00 2.11.2024

--- go/src/flow-control/main.go
+++ go/src/flow-control/main.go
@@ -9,10 +9,19 @@ import (
 
 func main() {
 	val := 0
-	for val <= 0 {
+
+	for {
 		fmt.Print("Enter number: ")
 		fmt.Scanf("%d", &val)
-		fmt.Println("You entered:", val)
+
+		switch {
+		case val < 0:
+			panic("You entered a negative number!")
+		case val == 0:
+			fmt.Println("0 is neither negative nor positive")
+		default:
+			fmt.Println("You entered:", val)
+		}
 	}
 }
 
