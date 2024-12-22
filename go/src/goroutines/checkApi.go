ca24baa45baa10f24477865a8b46f2a0cbb021b0
yuvallinux <yuvddd05@gmail.com>
2024-12-22 22:00:22 +0200

    22.12.2024 22:00

---
+++ go/src/goroutines/checkApi.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+
+	apis := []string{
+		"https://management.azure.com",
+		"https://dev.azure.com",
+		"https://api.github.com",
+		"https://outlook.office.com/",
+		"https://api.somewhereintheinternet.com/",
+		"https://graph.microsoft.com",
+	}
+
+	ch := make(chan string)
+
+	for _, api := range apis {
+		go checkApi(api, ch)
+	}
+
+	for i := 0; i < len(apis); i++ {
+		fmt.Print(<-ch)
+	}
+
+	elapsed := time.Since(start)
+	fmt.Printf("Done! It took %v seconds!\n", elapsed.Seconds())
+}
+
+func checkApi(api string, ch chan string) {
+
+	_, err := http.Get(api)
+	if err != nil {
+		ch <- fmt.Sprintf("ERROR: %s is down!\n", api)
+		return
+	}
+
+	ch <- fmt.Sprintf("SUCCESS: %s is up and running!\n", api)
+
+}
--- shell/traceroute.sh
+++ shell/traceroute.sh
@@ -4,15 +4,16 @@ for ttl in {1..128}
 do
     result=$(ping -c 1 -t $ttl -W 2 $1 2> /dev/null)
   
-    if [[ $result == *"FROM"*"Time to live exceeeded"* ]]; then
-      	echo $result | awk 'NR==2 {print $ttl" "$2}'
-    else
-	      echo "$ttl * * *"
-    fi
-
-    if echo "$result" | grep -q "time="; then
+    if [[ $result =~ "Time to live exceeded" ]]; then
+      	awk -v ttl="$ttl" 'NR==2 {print ttl" "$2}' <<< "$result"
+    
+    elif echo "$result" | grep -q "time="; then
+      	awk -v ttl="$ttl" 'NR==1 {print ttl" "$2}' <<< "$result"
         echo "Reached destination."
         break
-    fi
     
+    else
+        echo "$ttl * * *"
+    fi
+
 done
